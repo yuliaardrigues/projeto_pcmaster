80d6244b08f794ed920d481251e0bbee54a92d2c
Yulia <rodriguesyulia@aluno.unifapce.edu.br>
2025-05-07 19:35:24 -0300

    Atualiza o sistema de login para usar "username" em vez de "email". Adiciona novos campos ao modelo Produto e implementa a lógica para exibir produtos na página de detalhes. Cria template para a página de produtos e ajusta as URLs e views correspondentes.

--- web/models.py
+++ web/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 import datetime
+from datetime import timedelta
+from django.utils import timezone
+from django.utils.timezone import now
+
 
 
 class Categoria(models.Model):
@@ -20,6 +24,9 @@ class Produto(models.Model):
 
     sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+    data_fim_oferta = models.DateTimeField(null=True, blank=True)
+    nota = models.FloatField(default=0)
+
 
 
     def __str__(self):
@@ -30,6 +37,30 @@ class Produto(models.Model):
         if self.preco > 0 and self.sale_price < self.preco:
             return int(((self.preco - self.sale_price) / self.preco) * 100)
         return 0
+    
+  
+    @property
+    def tempo_restante(self):
+        if self.data_fim_oferta:
+            delta = self.data_fim_oferta - now()
+            if delta.total_seconds() > 0:
+                return {
+                    'dias': delta.days,
+                    'horas': delta.seconds // 3600,
+                    'minutos': (delta.seconds % 3600) // 60,
+                    'segundos': delta.seconds % 60,
+                }
+        return {'dias': 0, 'horas': 0, 'minutos': 0, 'segundos': 0}
+
+
+        
+        
+    
+    def estrelas_cheias(self):
+        return int(self.nota)
+
+    def estrelas_meia(self):
+        return self.nota - int(self.nota) >= 0.5
 
 
 #carrinho de compras 
--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('produto/', views.produto, name='produto'),
     
 ]
