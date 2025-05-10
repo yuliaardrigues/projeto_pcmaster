362a83cc674a2536139e669609ed9133021b5b31
Yulia <rodriguesyulia@aluno.unifapce.edu.br>
2025-05-10 11:43:24 -0300

    Adiciona a view e template para exibição de produtos. Atualiza o link de navegação na página inicial para direcionar corretamente para a página de produtos.

--- web/views.py
+++ web/views.py
@@ -5,3 +5,7 @@ def home(request):
     produto = Produto.objects.all()
     categorias = Categoria.objects.all()
     return render(request, 'home/home.html', {'produtos':produto, 'categorias': categorias} ) 
+
+def produto(request):
+    produto = Produto.objects.all()
+    return render(request, 'products/produto.html', {'produtos':produto})
